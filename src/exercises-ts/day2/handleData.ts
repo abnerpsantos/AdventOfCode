ec9d851f40003f646cf3fead512203402bee0b74
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2022-12-21 21:24:51 -0300

    Finished: Exercise (Rock Paper Scissors) from day 2, part one and two

--- src/exercises-ts/day2/handleData.ts
+++ src/exercises-ts/day2/handleData.ts
@@ -1,6 +1,27 @@
-import { getData } from '../../utils/getData';
+import { getData } from "../../utils/getData"
 
- export async function handleData() {
- const rawData = await getData('2')
- return rawData
+interface pointsForChooseProps {
+	[key: string]: number
 }
+const pointsForChoose: pointsForChooseProps = {
+	rock: 1,
+	paper: 2,
+	scissor: 3,
+}
+
+export async function handleData() {
+	const rawData = await getData("2")
+	const data = rawData
+		.split("\n")
+		.filter((round) => round != "")
+		.map((round) => round.split(" "))
+
+	return {
+		data,
+		pointsForChoose,
+	}
+}
+
+/*
+
+*/
