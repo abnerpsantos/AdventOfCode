04a7ef88107fed0f681671a7b782fbd8a25c4c5c
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2022-12-26 17:22:10 -0300

    Finished: Exercise (Tuning Trouble) from day 6, part one and two

--- src/exercises-ts/day06/handleData.ts
+++ src/exercises-ts/day06/handleData.ts
@@ -2,5 +2,6 @@ import { getData } from "../../utils/getData"
 
 export async function handleData() {
 	const rawData = await getData("06")
+	const data = rawData.split("")
 	return rawData
 }
--- src/exercises-ts/day06/partTwo.ts
+++ src/exercises-ts/day06/partTwo.ts
@@ -1,1 +1,21 @@
-export async function day06PartTwo() {}
+import { handleData } from "./handleData"
+
+export async function day06PartTwo() {
+	const data = await handleData()
+	const tmpArr = []
+	const markerLength = []
+	for (let i = 0; i < data.length; i++) {
+		tmpArr.push(data[i])
+		markerLength.push(data[i])
+		if (tmpArr.length === 14) {
+			const set = new Set(tmpArr)
+			if (set.size === 14) {
+				break
+			}
+			tmpArr.shift()
+		}
+	}
+	console.log(
+		`How many characters need to be processed before the first start-of-packet marker(14 distinct characters) is detected?\nThe puzzle answer is: ${markerLength.length}`
+	)
+}
